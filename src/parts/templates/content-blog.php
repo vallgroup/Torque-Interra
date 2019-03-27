71705272279211d67d266698f886847433a54e8f
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-26 18:50:35 -0600

    add pagination to filtered loop shortcodes

--- src/parts/templates/content-blog.php
+++ src/parts/templates/content-blog.php
@@ -2,6 +2,7 @@
 
 $shortcode = '[torque_filtered_loop ';
 $shortcode .= 'post_type="post" ';
+$shortcode .= 'posts_per_page="12" ';
 $shortcode .= 'filters_types="dropdown_tax,dropdown_date" ';
 $shortcode .= 'filters_args="category"';
 $shortcode .= ']';
--- src/parts/templates/content-listings.php
+++ src/parts/templates/content-listings.php
@@ -3,6 +3,7 @@
 if ( class_exists( 'Interra_Listing_CPT' ) ) {
   $shortcode = '[torque_filtered_loop ';
   $shortcode .= 'post_type="'.Interra_Listing_CPT::$listing_labels['post_type_name'].'" ';
+  $shortcode .= 'posts_per_page="12" ';
   $shortcode .= 'filters_types="tabs_acf,dropdown_tax,dropdown_tax" ';
   $shortcode .= 'filters_args="field_5c8ae924ce1a3,'.Interra_Listing_CPT::$LISTING_PROPERTY_TYPE_TAX_SLUG.','.Interra_Listing_CPT::$LISTING_REGION_TAX_SLUG.'"';
   $shortcode .= ']';
