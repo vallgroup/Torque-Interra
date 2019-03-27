e87776d7fd48b0ff096e802f39b557a40030d5ae
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-27 11:55:34 -0600

    add featured image to listing cpt region taxonomy

--- src/includes/cpts/interra-child-listing-cpt-class.php
+++ src/includes/cpts/interra-child-listing-cpt-class.php
@@ -242,6 +242,54 @@ class Interra_Listing_CPT {
       	'description' => '',
       ));
 
+      acf_add_local_field_group(array(
+      	'key' => 'group_5c9ba4e29e924',
+      	'title' => 'Featured Image',
+      	'fields' => array(
+      		array(
+      			'key' => 'field_5c9ba4ec433cd',
+      			'label' => 'Image',
+      			'name' => 'region_featured_image',
+      			'type' => 'image',
+      			'instructions' => '',
+      			'required' => 0,
+      			'conditional_logic' => 0,
+      			'wrapper' => array(
+      				'width' => '',
+      				'class' => '',
+      				'id' => '',
+      			),
+      			'return_format' => 'url',
+      			'preview_size' => 'thumbnail',
+      			'library' => 'all',
+      			'min_width' => '',
+      			'min_height' => '',
+      			'min_size' => '',
+      			'max_width' => '',
+      			'max_height' => '',
+      			'max_size' => '',
+      			'mime_types' => '',
+      		),
+      	),
+      	'location' => array(
+      		array(
+      			array(
+      				'param' => 'taxonomy',
+      				'operator' => '==',
+      				'value' => self::$LISTING_REGION_TAX_SLUG,
+      			),
+      		),
+      	),
+      	'menu_order' => 0,
+      	'position' => 'normal',
+      	'style' => 'default',
+      	'label_placement' => 'top',
+      	'instruction_placement' => 'label',
+      	'hide_on_screen' => '',
+      	'active' => 1,
+      	'description' => '',
+      ));
+
       endif;
   }
 }
