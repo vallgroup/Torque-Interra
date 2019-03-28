129471d339c2c824ad0d7a5729e136a9a7718a32
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-28 17:04:02 -0600

    add field to link a staff member to a wp user

--- src/includes/cpts/interra-child-staff-cpt-class.php
+++ src/includes/cpts/interra-child-staff-cpt-class.php
@@ -6,6 +6,7 @@ class Interra_Staff_CPT {
 
   public function __construct() {
     add_action( 'init', array( $this, 'add_staff_taxonomies' ) );
+    add_action('acf/init', array( $this, 'add_acf_metaboxes' ) );
     add_filter( Torque_Staff_CPT::$STAFF_METABOXES_FILTER_HOOK, array( $this, 'exclude_metaboxes' ) );
   }
 
@@ -26,6 +27,54 @@ class Interra_Staff_CPT {
   public function exclude_metaboxes() {
 
     return array('role');
+  }
+
+  public function add_acf_metaboxes() {
+    if( function_exists('acf_add_local_field_group') ):
+
+      acf_add_local_field_group(array(
+      	'key' => 'group_5c9d4fca4fc93',
+      	'title' => 'User Relationship',
+      	'fields' => array(
+      		array(
+      			'key' => 'field_5c9d4fd222c78',
+      			'label' => 'User',
+      			'name' => 'user',
+      			'type' => 'user',
+      			'instructions' => 'Pick a WP user to associate with this staff member. This allows us to display a staff member\'s blog posts by checking the author.',
+      			'required' => 0,
+      			'conditional_logic' => 0,
+      			'wrapper' => array(
+      				'width' => '',
+      				'class' => '',
+      				'id' => '',
+      			),
+      			'role' => '',
+      			'allow_null' => 0,
+      			'multiple' => 0,
+      			'return_format' => 'id',
+      		),
+      	),
+      	'location' => array(
+      		array(
+      			array(
+      				'param' => 'post_type',
+      				'operator' => '==',
+      				'value' => 'torque_staff',
+      			),
+      		),
+      	),
+      	'menu_order' => 5,
+      	'position' => 'normal',
+      	'style' => 'default',
+      	'label_placement' => 'top',
+      	'instruction_placement' => 'label',
+      	'hide_on_screen' => '',
+      	'active' => 1,
+      	'description' => '',
+      ));
+
+      endif;
   }
 }
 
