24069029fdaf8d11dd3e07e31b9bb773e6a449aa
mattwills8 <matt_wills8@hotmail.co.uk>
2019-04-03 12:53:56 -0600

    add keep reading to single template

--- src/parts/templates/content-post.php
+++ src/parts/templates/content-post.php
@@ -1,6 +1,13 @@
 <?php
 
 $terms = get_the_terms( get_the_ID(), 'category' );
+$term_ids = array_map( function($term) { return $term->term_id; }, $terms);
+
+$keep_reading_query = new WP_Query( array(
+  'posts_per_page'    => 3,
+  'post__not_in'      => [get_the_ID()],
+  'category__in'      => $term_ids
+) );
 
 ?>
 
@@ -31,7 +38,16 @@ $terms = get_the_terms( get_the_ID(), 'category' );
     <?php get_template_part('/parts/elements/pagination/pagination-buttons-single'); ?>
   </div>
 
-  <div class="post-keep-reading" >
+  <?php  if ( $keep_reading_query->have_posts() ) { ?>
+    <div class="post-keep-reading" >
+      <h3>Keep Reading</h3>
 
-  </div>
+    <?php while ( $keep_reading_query->have_posts() ) { $keep_reading_query->the_post();
+      get_template_part('parts/shared/loop-blog');
+    } ?>
+    </div>
+
+  <?php
+  wp_reset_postdata();
+  } ?>
 </div>
