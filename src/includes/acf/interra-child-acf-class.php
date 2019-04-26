82fb53b43c0af81158bd3c2e169af249b2d1d941
mattwills8 <matt_wills8@hotmail.co.uk>
2019-04-26 09:57:47 -0600

    add post additional authors acf field

--- src/includes/acf/interra-child-acf-class.php
+++ src/includes/acf/interra-child-acf-class.php
@@ -37,6 +37,48 @@ class Interra_ACF {
     if( function_exists('acf_add_local_field_group') ):
 
       acf_add_local_field_group(array(
+      	'key' => 'group_5cc3297fda429',
+      	'title' => 'Post Authors',
+      	'fields' => array(
+      		array(
+      			'key' => 'field_5cc32988eb3b0',
+      			'label' => 'Additional Authors',
+      			'name' => 'post_authors',
+      			'type' => 'user',
+      			'instructions' => '',
+      			'required' => 0,
+      			'conditional_logic' => 0,
+      			'wrapper' => array(
+      				'width' => '',
+      				'class' => '',
+      				'id' => '',
+      			),
+      			'role' => '',
+      			'allow_null' => 0,
+      			'multiple' => 1,
+      			'return_format' => 'id',
+      		),
+      	),
+      	'location' => array(
+      		array(
+      			array(
+      				'param' => 'post_type',
+      				'operator' => '==',
+      				'value' => 'post',
+      			),
+      		),
+      	),
+      	'menu_order' => 0,
+      	'position' => 'side',
+      	'style' => 'default',
+      	'label_placement' => 'top',
+      	'instruction_placement' => 'label',
+      	'hide_on_screen' => '',
+      	'active' => 1,
+      	'description' => '',
+      ));
+
+      acf_add_local_field_group(array(
       	'key' => 'group_5c954a9073a65',
       	'title' => 'Page Hero',
       	'fields' => array(
