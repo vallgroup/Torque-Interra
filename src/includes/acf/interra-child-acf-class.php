f9543391659e970c07b6e70ecadd9f550331f848
Al Notara <al@vallgroup.com>
2019-05-09 11:25:24 -0700

    Added ability to add/remove/sort job titles

--- src/includes/acf/interra-child-acf-class.php
+++ src/includes/acf/interra-child-acf-class.php
@@ -945,7 +945,71 @@ class Interra_ACF {
       	'hide_on_screen' => '',
       	'active' => 1,
       	'description' => '',
-      ));
+		));
+		
+		acf_add_local_field_group(array(
+			'key' => 'group_5cd44dac63578',
+			'title' => 'Staff Details',
+			'fields' => array(
+				array(
+					'key' => 'field_5cd44dd099191',
+					'label' => 'Job Titles',
+					'name' => 'staff_job_titles',
+					'type' => 'repeater',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'collapsed' => '',
+					'min' => 0,
+					'max' => 0,
+					'layout' => 'table',
+					'button_label' => 'Add a Job Title',
+					'sub_fields' => array(
+						array(
+							'key' => 'field_5cd44e82fd6d5',
+							'label' => 'Job Title',
+							'name' => 'staff_job_title',
+							'type' => 'text',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array(
+								'width' => '',
+								'class' => '',
+								'id' => '',
+							),
+							'default_value' => '',
+							'placeholder' => '',
+							'prepend' => '',
+							'append' => '',
+							'maxlength' => '',
+						),
+					),
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'options_page',
+						'operator' => '==',
+						'value' => 'acf-options',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => 1,
+			'description' => '',
+		));
 
       acf_add_local_field_group(array(
       	'key' => 'group_5c98f5f420ccb',
--- src/parts/acf/modules/staff-members.php
+++ src/parts/acf/modules/staff-members.php
@@ -1,21 +1,21 @@
 <?php
 
 $sorted_users = array();
-$search_terms = array(
-  'co-founder, managing principal',
-  'senior managing partner',
-  'managing partner',
-  'director',
-  'associate',
-  'office manager',
-  'marketing director'
-);
 
-foreach ( $search_terms as $search_term ) {
+/* Check the ACF for rows */
+if( have_rows('staff_job_titles', 'options') ):
+  
+  /* Loop through any existing rows */
+  while ( have_rows('staff_job_titles', 'options') ) : the_row();
+
+  //$job_title_id = get_row_index();
+  $job_title = get_sub_field('staff_job_title', 'options');
+
+  /* Setup arguments for WP DB Query */
   $args = array(
     'role__in' => array( Interra_Roles::$BROKER_ROLE_SLUG, Interra_Roles::$MANAGER_ROLE_SLUG ),
     'meta_key'      => 'job_title',
-    'meta_value'    => $search_term,
+    'meta_value'    => $job_title,
     'meta_compare'  => '=',
     'orderby'       => 'display_name',
     'order'         => 'ASC'
@@ -24,10 +24,27 @@ foreach ( $search_terms as $search_term ) {
   $user_query = new WP_User_Query( $args );
 
   if ( ! empty( $user_query->get_results() ) ) {
+    /* Append results to end of sorted users array */
     $sorted_users = array_merge( $sorted_users, $user_query->get_results() );
   }
 
-} ?>
+  endwhile;
+
+else :
+
+  /* No AFC job titles added to system, therefore just query the users in alphabetical order */
+  $args = array(
+    'role__in' => array( Interra_Roles::$BROKER_ROLE_SLUG, Interra_Roles::$MANAGER_ROLE_SLUG ),
+    'orderby'       => 'display_name',
+    'order'         => 'ASC'
+  );
+
+  $sorted_users = new WP_User_Query( $args );
+  $sorted_users = $sorted_users->get_results();
+
+endif;
+
+if ( ! empty( $sorted_users ) ) { ?>
 
   <div class="staff-members-module" >
 
@@ -91,4 +108,4 @@ foreach ( $search_terms as $search_term ) {
 
   </div>
 
-<?php /* } */ ?>
+<?php } ?>
