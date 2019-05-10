f778aa31fd18f9571936e3a7cffb22b1c9b695e7
Al Notara <al@vallgroup.com>
2019-05-10 15:24:51 -0700

    Future-proofed quick search defaulting to 'availble'

--- src/parts/acf/modules/quick-search.php
+++ src/parts/acf/modules/quick-search.php
@@ -1,5 +1,6 @@
 <?php
 
+$listing_status_key = 'field_5c8ae924ce1a3';
 $term_ids = get_sub_field('regions');
 
 if ($term_ids) {
@@ -12,6 +13,22 @@ if ($term_ids) {
   $terms = [];
 }
 
+/**
+ * Get a random 'torque_listing' post, then its ID.
+ * Then use the ID to get ACF listing_status key value (req. post ID)
+ * ID not important, as we're only looking for the key value, not the actual ACF value
+ */
+$args = array( 
+  'orderby' => 'rand',
+  'posts_per_page' => '1', 
+  'post_type' => 'torque_listing'
+);
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+  $listing_post_id = get_the_ID();
+  $listing_status_key = get_field_object('listing_status', $listing_post_id);
+  $listing_status_key = $listing_status_key['key'];
+endwhile;
 
 if (count($terms)) {
 
@@ -24,8 +41,9 @@ if (count($terms)) {
 <?php foreach ($terms as $term) {
 
   $image = get_field('region_featured_image', $term);
+  /* 'field_5c8ae924ce1a3' is the field key for 'listing_status' */
   ?>
-  <a class="quick-search-term-wrapper" href="/listings?<?php echo $taxonomy.'='.$term->term_id; ?>&field_5c8ae924ce1a3=available" >
+  <a class="quick-search-term-wrapper" href="/listings?<?php echo $taxonomy.'='.$term->term_id; ?>&<?php echo $listing_status_key; ?>=available" >
     <div class="quick-search-term" style="background-image: url('<?php echo $image; ?>')">
       <div class="term-name"><?php echo $term->name; ?></div>
 
