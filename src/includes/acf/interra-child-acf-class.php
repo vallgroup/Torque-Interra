205175508b287a37d55646f37c0caefe836e9322
Al Notara <al@vallgroup.com>
2019-05-20 18:10:35 -0700

    Added ACF Social Media options to Options page

--- src/includes/acf/interra-child-acf-class.php
+++ src/includes/acf/interra-child-acf-class.php
@@ -1069,7 +1069,97 @@ class Interra_ACF {
       	'hide_on_screen' => '',
       	'active' => 1,
       	'description' => '',
-      ));
+		));
+		
+		acf_add_local_field_group(array(
+			'key' => 'group_5ce3438132e73',
+			'title' => 'Social Media',
+			'fields' => array(
+				array(
+					'key' => 'field_5ce34402d4dab',
+					'label' => 'Social Media',
+					'name' => 'social_media',
+					'type' => 'repeater',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'collapsed' => '',
+					'min' => 0,
+					'max' => 0,
+					'layout' => 'table',
+					'button_label' => '',
+					'sub_fields' => array(
+						array(
+							'key' => 'field_5ce34426d4dac',
+							'label' => 'Social Channel',
+							'name' => 'social_channel',
+							'type' => 'select',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array(
+								'width' => '',
+								'class' => '',
+								'id' => '',
+							),
+							'choices' => array(
+								'twitter' => 'Twitter',
+								'linkedin' => 'LinkedIn',
+								'instagram' => 'Instagram',
+								'facebook' => 'Facebook',
+								'youtube' => 'YouTube',
+							),
+							'default_value' => array(
+							),
+							'allow_null' => 0,
+							'multiple' => 0,
+							'ui' => 0,
+							'return_format' => 'value',
+							'ajax' => 0,
+							'placeholder' => '',
+						),
+						array(
+							'key' => 'field_5ce34444d4dad',
+							'label' => 'Social URL',
+							'name' => 'social_url',
+							'type' => 'url',
+							'instructions' => '',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array(
+								'width' => '',
+								'class' => '',
+								'id' => '',
+							),
+							'default_value' => '',
+							'placeholder' => '',
+						),
+					),
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'options_page',
+						'operator' => '==',
+						'value' => 'acf-options',
+					),
+				),
+			),
+			'menu_order' => 0,
+			'position' => 'normal',
+			'style' => 'default',
+			'label_placement' => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen' => '',
+			'active' => 1,
+			'description' => '',
+		));
 
       endif;
   }
