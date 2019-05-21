bbdd4531ee10bb6a58b8a21e4c26cebde5060815
Al Notara <al@vallgroup.com>
2019-05-21 15:55:34 -0700

    Fix: 'load more' bug on author/team pages for blog posts and listings regarding permalinks

--- src/parts/shared/loop-blog.php
+++ src/parts/shared/loop-blog.php
@@ -1,24 +1,30 @@
+<?php
+/**
+ * This template is used by the 'Load More' functionality (/torque-theme/includes/load-more/).
+ * This helper currently doesn't play ball with 'echo get_the_permalink();' embedded in HTML, therefore all instances of the permalink have been wrapped in PHP and echo'd in their entirety...
+ */
+?>
 <div class="loop-post loop-blog">
-  <a href=<?php echo get_the_permalink(); ?>>
+  <?php echo '<a href="' . get_the_permalink() . '">'; ?>
     <div class="featured-image-wrapper">
       <div
         class="featured-image"
         style="background-image: url('<?php echo get_the_post_thumbnail_url(null, 'medium'); ?>')"
       ></div>
     </div>
-  </a>
+  <?php echo '</a>'; ?>
 
   <div class="content-wrapper">
-    <a href=<?php echo get_the_permalink(); ?>>
+    <?php echo '<a href="' . get_the_permalink() . '">'; ?>
       <h4><?php echo get_the_title(); ?></h4>
-    </a>
+    <?php echo '</a>'; ?>
 
     <div class="excerpt" >
       <?php echo get_the_excerpt(); ?>
     </div>
 
-    <a href=<?php echo get_the_permalink(); ?>>
+    <?php echo '<a href="' . get_the_permalink() . '">'; ?>
       <button>Read More</button>
-    </a>
+    <?php echo '</a>'; ?>
   </div>
 </div>
