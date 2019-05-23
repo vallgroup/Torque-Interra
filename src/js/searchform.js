5e733b9912be0e5a5a04779c04d9c4e943ce8880
Al Notara <al@vallgroup.com>
2019-05-23 15:18:12 -0700

    Focus on search form input when search form is opened

--- src/js/searchform.js
+++ src/js/searchform.js
@@ -2,12 +2,17 @@
   $(document).ready(() => {
     const headerContent = $("header .torque-header-content-wrapper");
     const headerSearchForm = headerContent.find("form.searchform");
+    const headerSearchFormInput = headerSearchForm.find("input");
     const outsideSearchForm = $("main");
 
     if (headerContent && headerSearchForm) {
       headerSearchForm.click(function(e) {
         headerContent.toggleClass("searchform-open");
         $(this).toggleClass("closed");
+        // If we've opened the search form then forcus on it...
+        if ( headerContent.hasClass("searchform-open") ) {
+          headerSearchFormInput.focus();
+        }
       });
 
       headerSearchForm.find("input").click(function(e) {
