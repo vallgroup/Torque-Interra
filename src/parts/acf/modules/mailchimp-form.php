b2083ba55b602ea761d83159deff229fe505eb93
Mario Vallejo <mario@vallgroup.com>
2019-12-09 13:32:35 -0600

    update mailchimp to use icontact

--- src/parts/acf/modules/mailchimp-form.php
+++ src/parts/acf/modules/mailchimp-form.php
@@ -11,18 +11,20 @@ $desc = get_sub_field( 'mailchimp_desc' );
 
 <!-- Begin Mailchimp Signup Form -->
 <!-- <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css"> -->
-<style type="text/css">
+<!-- <style type="text/css">
   #mc_embed_signup{background:#fff; clear:left; }
-
-
 </style>
 <div id="mc_embed_signup">
   <form action="<?php echo $action; ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
       <div id="mc_embed_signup_scroll">
 
-        <h2><?php echo $title; ?></h2>
+        <h2>
+          <?php echo $title; ?>
+        </h2>
 
-        <p><?php echo $desc; ?></p>
+        <p>
+          <?php echo $desc; ?>
+        </p>
 
         <div class="mc-field-group first">
           <input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name">
@@ -32,13 +34,14 @@ $desc = get_sub_field( 'mailchimp_desc' );
           <input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name">
         </div>
 
-        <div style="clear: both;"></div>
+        <div style="clear: both;">
+        </div>
 
         <div class="mc-field-group email">
           <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
         </div>
 
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+
         <div style="position: absolute; left: -5000px;" aria-hidden="true">
           <input type="text" name="b_ad84318294be29bb41e051167_f999becc29" tabindex="-1" value="">
         </div>
@@ -47,20 +50,86 @@ $desc = get_sub_field( 'mailchimp_desc' );
           <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button">
         </div>
 
-        <div style="clear: both;"></div>
+        <div style="clear: both;">
+        </div>
 
         <div id="mce-responses" class="clear">
-          <div class="response" id="mce-error-response" style="display:none"></div>
-          <div class="response" id="mce-success-response" style="display:none"></div>
+          <div class="response" id="mce-error-response" style="display:none">
+          </div>
+          <div class="response" id="mce-success-response" style="display:none">
+          </div>
         </div>
 
       </div>
   </form>
 </div>
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'>
+</script>
 <script type='text/javascript'>
 (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);
-</script>
+</script> -->
 <!--End mc_embed_signup-->
+
+<!-- iContact Form -->
+<div id="mc_embed_signup">
+<script type="text/javascript" src="//app.icontact.com/icp/static/form/javascripts/validation-captcha.js">
+</script>
+
+<script type="text/javascript" src="//app.icontact.com/icp/static/form/javascripts/tracking.js">
+</script>
+
+<link rel="stylesheet" type="text/css" href="//app.icontact.com/icp/static/human/css/signupBuilder/formGlobalStyles.css">
+
+<style type="text/css" id="signupBuilderAdvancedStyles">
+
+</style>
+
+<style type="text/css" id="signupBuilderStyles"></style>
+
+<form id="mc-embedded-subscribe-form" captcha-key="6LeCZCcUAAAAALhxcQ5fN80W6Wa2K3GqRQK6WRjA" captcha-theme="light" new-captcha="true" method="POST" action="https://app.icontact.com/icp/core/mycontacts/signup/designer/form/?id=412&cid=576446&lid=18679">
+  <div id="mc_embed_signup_scroll">
+
+    <h2>
+      <?php echo $title; ?>
+    </h2>
+
+    <p>
+      <?php echo $desc; ?>
+    </p>
+
+    <div class="mc-field-group first" data-validation-type="1" data-label="First Name">
+      <input type="text" name="data[fname]" placeholder="First Name">
+    </div>
+    <div class="mc-field-group last" data-validation-type="1" data-label="Last Name">
+      <input type="text" name="data[lname]" placeholder="Last Name">
+    </div>
+
+    <div style="clear: both;">
+    </div>
+
+    <div class="mc-field-group email" data-validation-type="1" data-label="Email">
+      <input type="text" name="data[email]" placeholder="Email Address">
+    </div>
+
+    <div class="clear submit-btn">
+      <input type="submit" value="Submit" id="mc-embedded-subscribe" class="button">
+    </div>
+
+    <div style="clear: both;">
+    </div>
+
+<div class="formEl fieldtype-checkbox required" dataname="listGroups" data-validation-type="1" data-label="Lists" style="display: none; width: 100%;">
+<h3>Lists<span class="indicator required">*</span>
+</h3>
+<div class="option-container">
+<label class="checkbox">
+<input type="checkbox" alt="" name="data[listGroups][]" value="789300" checked="checked">Eric Biel</label>
+</div>
+</div>
+    </div>
+  </form>
+  <img src="//app.icontact.com/icp/core/signup/tracking.gif?id=null&cid=576446&lid=18679"/>
+</div>
+<!-- End iContact Form -->
 
 </div>
