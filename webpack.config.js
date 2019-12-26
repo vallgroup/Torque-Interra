a259a48edc0028296e555bcc37b6f09829542c49
Mario Vallejo <mario@vallgroup.com>
2019-12-26 13:54:11 -0500

    fix issue with statics dir

--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,14 @@ const config = {
     rules: [
       {
         test: projectConfig.webpackDefaults.js.test,
-        exclude: ["/node_modules/", "statics"],
+        exclude: ["/node_modules/", "/src/statics"],
         use: {
           loader: "babel-loader"
         }
       },
       {
         test: projectConfig.webpackDefaults.css.test,
-        exclude: ["/node_modules/", "statics"],
+        exclude: ["/node_modules/", "/src/statics"],
         use: ExtractTextPlugin.extract({
           fallback: "style-loader?sourceMap",
           use: projectConfig.webpackDefaults.css.loaders
@@ -42,7 +42,7 @@ const config = {
       },
       {
         test: projectConfig.webpackDefaults.scss.test,
-        exclude: ["/node_modules/", "statics"],
+        exclude: ["/node_modules/", "/src/statics"],
         use: ExtractTextPlugin.extract({
           fallback: "style-loader?sourceMap",
           // resolve-url-loader may be chained before sass-loader if necessary
@@ -51,12 +51,12 @@ const config = {
       },
       {
         test: projectConfig.webpackDefaults.images.test,
-        exclude: ["/node_modules/", "statics"],
+        exclude: ["/node_modules/", "/src/statics"],
         use: projectConfig.webpackDefaults.images.loaders
       },
       {
         test: projectConfig.webpackDefaults.fonts.test,
-        exclude: ["/node_modules/", "statics"],
+        exclude: ["/node_modules/", "/src/statics"],
         use: projectConfig.webpackDefaults.fonts.loaders
       }
     ]
@@ -71,7 +71,7 @@ const config = {
     }),
     new CopyWebpackPlugin([
       {
-        from: path.join(srcDir, "statics/**/*"),
+        from: path.join(srcDir, "/src/statics/**/*"),
         to: buildDir
       },
       { from: path.join(srcDir, "style.css"), to: buildDir },
