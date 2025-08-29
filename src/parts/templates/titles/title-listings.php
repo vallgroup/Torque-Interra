c95c1c9333cff4711ecf46530a773a48dbc61cec
Jorge Cortez <jorge@vallgroup.com>
2025-08-29 12:15:31 -0400

    feat: add properties page

--- src/parts/templates/titles/title-listings.php
+++ src/parts/templates/titles/title-listings.php
@@ -1,11 +1,20 @@
 <?php
 
-get_template_part('parts/shared/page-hero-default');
-
 $heading = get_field('custom_template_heading');
 
+//get the featured image from the post
+$image = get_the_post_thumbnail_url();
 ?>
 
-<div class="listings-title" >
-  <h1><?php echo $heading; ?></h1>
+<div class="page-hero type-image">
+
+  <div class="hero-image-size">
+    <div class="hero-overlay-bg"></div>
+    <div class="hero-image" style="background-image: url(<?php echo $image; ?>);"></div>
+  </div>
+
+  <div class="hero-overlay">
+    <h1><?php echo $heading; ?></h1>
+    <div class="hero-subtitle"><?php the_title(); ?></div>
+  </div>
 </div>
