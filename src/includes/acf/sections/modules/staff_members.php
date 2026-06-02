2868f82f8a018d4be3a07a2712ee8692a8162ad7
Jorge Cortez <jorge@vallgroup.com>
2026-06-02 16:12:16 -0400

    feat: add heading to the staff member component

--- src/includes/acf/sections/modules/staff_members.php
+++ src/includes/acf/sections/modules/staff_members.php
@@ -5,7 +5,27 @@ return array(
     'name' => 'staff_members',
     'label' => 'Staff Members',
     'display' => 'block',
-    'sub_fields' => array(),
+    'sub_fields' => array(
+        array(
+            'key' => 'field_5c9d3f4702dd0',
+            'label' => 'Heading',
+            'name' => 'heading',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+    ),
     'min' => '',
     'max' => '',
 );
